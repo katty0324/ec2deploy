100d67680f9ff91aaebafb675966653a2617c2df
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2013-09-27 00:54:05 +0900

    Implement Deployer#describeInstance

--- ec2deploy.php
+++ ec2deploy.php
@@ -4,5 +4,5 @@ require_once dirname(__FILE__) . '/../aws-sdk-for-php/sdk.class.php';
 require_once dirname(__FILE__) . '/config.inc.php';
 require_once dirname(__FILE__) . '/src/Deployer.php';
 
-$deployer = new Deployer(AmazonELB::REGION_APAC_NE1);
+$deployer = new Deployer('ap-northeast-1');
 $deployer->deploy($argv[1], $argv[2]);
--- src/Deployer.php
+++ src/Deployer.php
@@ -3,16 +3,34 @@
 class Deployer {
 
 	private $amazonELB;
+	private $amazonEC2;
 
 	public function __construct($region) {
+		
 		$this->amazonELB = new AmazonELB();
-		$this->amazonELB->set_region($region);
+		$this->amazonEC2 = new AmazonEC2();
+		$this->amazonELB->set_region("elasticloadbalancing.${region}.amazonaws.com");
+		$this->amazonEC2->set_region("ec2.${region}.amazonaws.com");
+
 	}
 
 	public function deploy($elbName, $command) {
 
 		$instances = $this->listInstances($elbName);
-		
+
+		foreach ($instances as $instance) {
+
+			while (!$this->isHealthy($instances))
+				sleep(5);
+
+			// TODO deregister instance
+
+			$instance = $this->describeInstance($instance->InstanceId->to_string());
+
+			// TODO register instance
+
+		}
+
 	}
 
 	private function isHealthy($instances) {
@@ -36,6 +54,17 @@ class Deployer {
 			throw new Exception($response->body->Error->Message);
 
 		return $response->body->DescribeInstanceHealthResult->InstanceStates->member;
+
+	}
+
+	private function describeInstance($instanceId) {
+
+		$response = $this->amazonEC2->describe_instances(array('InstanceId' => $instanceId));
+
+		if (!$response->isOK())
+			throw new Exception($response->body->Error->Message);
+
+		return $response->body->reservationSet->item;
 
 	}
 
