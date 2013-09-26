1873da5b3a4fa04aaa07cadd1c817c55807af649
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2013-09-26 19:57:33 +0900

    Implement Deployer#listInstances

--- ec2deploy.php
+++ ec2deploy.php
@@ -1,3 +1,8 @@
 <?php
 
+require_once dirname(__FILE__) . '/../aws-sdk-for-php/sdk.class.php';
+require_once dirname(__FILE__) . '/config.inc.php';
 require_once dirname(__FILE__) . '/src/Deployer.php';
+
+$deployer = new Deployer(AmazonELB::REGION_APAC_NE1);
+$deployer->deploy($argv[1], $argv[2]);
--- src/Deployer.php
+++ src/Deployer.php
@@ -2,4 +2,23 @@
 
 class Deployer {
 
+	private $amazonELB;
+
+	public function __construct($region) {
+		$this->amazonELB = new AmazonELB();
+		$this->amazonELB->set_region($region);
+	}
+
+	public function deploy($elbName, $command) {
+
+		$this->listInstances($elbName);
+
+	}
+
+	private function listInstances($elbName) {
+
+		return $this->amazonELB->describe_instance_health($elbName);
+
+	}
+
 }
